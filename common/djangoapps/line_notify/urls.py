b238036fa88d7101a822bc8596ad52a529acf67f
Anukul Thienkasemsuk <anukul_thienkasemsuk@hotmail.com>
2019-07-30 23:19:20 +0700

    fix reverse not found

--- common/djangoapps/line_notify/urls.py
+++ common/djangoapps/line_notify/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^line-login', line_login),
     url(r'^revoke_token', revoke_token),
     url(r'^callback', callback),
-    url(r'^complete', complete),
+    url(r'^complete', complete, name='complete'),
     url(r'^post_message', post_message),
     url(r'^check_status', check_status)
 ]
